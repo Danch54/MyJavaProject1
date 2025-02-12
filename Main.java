6f351ddc67c3ca4ad732855b31fd724ee2913f9b
Danch54 <dan.alex2212@mail.ru>
2025-02-12 22:43:38 +0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,21 +1,8 @@
+package MyPackage;
+
 public class Main {
-
     public static void main(String[] args) {
-        // Объявление переменных
-        int age = 32;                   // Целое число
-        double salary = 100000.00;      // Число с плавающей точкой
-        String name = "Daniil";         // Текстовая строка
-        boolean isEmployed = true;      // Логическое значение
-
-        // Вывод переменных на консоль
-        System.out.println("Имя: " + name);          // Вывод имени
-        System.out.println("Возраст: " + age);        // Вывод возраста
-        System.out.println("Зарплата: " + salary);    // Вывод зарплаты
-        System.out.println("Трудоустроен: " + isEmployed); // Вывод статуса трудоустройства
-    } 
+        Animal dog = new Animal("Гросс", "woof");
+        dog.speak();
+    }
 }
-
-
-    
-
-    
